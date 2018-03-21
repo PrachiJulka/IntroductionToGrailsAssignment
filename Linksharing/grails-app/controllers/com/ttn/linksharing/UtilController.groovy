50fe049223779f8771358b3dc99b002e62c680e8
Prachi <prachijulka31@gmail.com>
2018-03-21 20:29:00 +0530

    Question11

--- Linksharing/grails-app/controllers/com/ttn/linksharing/UtilController.groovy
+++ Linksharing/grails-app/controllers/com/ttn/linksharing/UtilController.groovy
@@ -1,12 +1,15 @@
 package com.ttn.linksharing
 
+import org.jboss.logging.Param
+
 class UtilController {
 
     def index() {
         log.info 'from log info'
         log.warn 'from log warn'
         log.trace('Hello')
-
+        log.debug 'Params Example [{}]', params
+        [param: params]
         //return index()
 
     }
